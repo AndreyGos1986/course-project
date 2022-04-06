d752a7e9ea9856bf4bac29cede9ca29a8e0894b6
Андрей <gosudarev_a@mail.ru>
2022-04-06 08:09:52 +0300

    Курсовая в лёгкой версии.

---
+++ src/Employee.java
@@ -0,0 +1,63 @@
+public class Employee {
+    static int counter = 1;
+    int id;
+    String firstName;
+    String middleName;
+    String lastName;
+    double sallary;
+    int deptNum;
+
+
+    public void setSallary(double sallary) {
+        this.sallary = sallary;
+    }
+
+    public void setDéptNum(int déptNum) {
+        this.deptNum = déptNum;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getSallary() {
+        return sallary;
+    }
+
+    public int getDeptNum() {
+        return deptNum;
+    }
+
+    public Employee(int id, String firstName, String middleName,
+                    String lastName, double sallary, int deptNum) {
+        this.id = counter;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.sallary = sallary;
+        this.deptNum = deptNum;
+    }
+
+    public static int getCounter() {
+        return counter++;
+    }
+
+
+
+}
+
+
+
